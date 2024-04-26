46834aa761cd13037a170215541285547ac9cd35
João Domingos Pinheiro Neto <56659658+joaonetogit@users.noreply.github.com>
2024-04-26 12:59:24 -0300

    feat: install vitest, eslint (airbnb) + prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx'],
+      },
+    },
+  },
+  extends: ['airbnb', 'prettier', 'plugin:prettier/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', 'prettier'],
+  rules: {},
+};
---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,8 @@
+import '@testing-library/jest-dom';
+import { describe, expect, it } from 'vitest';
+
+describe('Renders main page correctly', async () => {
+  it('Should render the page correctly', async () => {
+    expect(true).toBeTruthy();
+  });
+});
